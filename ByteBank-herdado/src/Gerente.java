364300dae2e69e248296e24271ace8e00f1edbae
Raian Medeiros Nolaço <raiannolaco@gmail.com>
2021-08-16 23:50:08 -0300

    .

---
+++ ByteBank-herdado/src/Gerente.java
@@ -0,0 +1,33 @@
+
+//gerente é um funcionario, gerente herda da classe funcionario
+public class Gerente extends Funcionario {
+
+	private int senha;
+
+	public Gerente() {
+
+	}
+
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+
+	public boolean autentica(int senha) {
+
+		if (this.senha == senha) {
+
+			return true;
+
+		} else
+			return false;
+
+	}
+	
+	 
+	
+	public double getBonificacao(){
+		return this.salario;
+	}
+	   
+	 
+}
---
+++ ByteBank-herdado/src/TesetaGerente.java
@@ -0,0 +1,23 @@
+
+public class TesetaGerente {
+
+	public static void main(String[] args) {
+		
+		Gerente g1  =  new Gerente();
+		
+		g1.setNome("Raian");
+		g1.setCpf("1234567");
+		g1.setSalario(5000);
+
+		System.out.println(g1.getNome());
+		System.out.println(g1.getCpf());
+		System.out.println(g1.getSalario());
+		
+		g1.setSenha(2222);
+		
+		boolean autentica = g1.autentica(2222);
+		
+		System.out.println(autentica);
+	}
+
+}
