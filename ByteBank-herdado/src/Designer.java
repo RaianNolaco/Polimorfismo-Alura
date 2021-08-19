e289f142041e24c9be98b38bebfbae52753b2bef
Raian Medeiros Nolaço <raiannolaco@gmail.com>
2021-08-19 01:22:02 -0300

    .

---
+++ ByteBank-herdado/src/Designer.java
@@ -0,0 +1,10 @@
+
+public class Designer extends Funcionario{
+	
+	public double getBonificacao() {
+	
+		System.out.println("Chamando bonificação do Designer");
+		return 200;
+		
+	} 
+}
--- ByteBank-herdado/src/TesteReferencias.java
+++ ByteBank-herdado/src/TesteReferencias.java
@@ -14,11 +14,15 @@ public class TesteReferencias {
 		ControleBonificacao controle = new ControleBonificacao();
 		Funcionario f1 = new Funcionario();
 		EditorVideo ev1 =  new EditorVideo();
+		Designer d1 =  new Designer();
+		
 		
 		g1.setSalario(5000);
 		f1.setSalario(2000);
 		ev1.setSalario(2500);
+		d1.setSalario(2000);
 		
+		controle.registra(d1);
 		controle.registra(g1);
 		controle.registra(f1);
 		controle.registra(ev1);
